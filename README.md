3398ab157621389a206113baa32234d4f3ca4612
Samuel Coe <samuel.m.coe@gmail.com>
2021-06-28 14:42:29 -0600

    Initial script and config

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # localizejs-github-action
+
 A Github Actions integration for Localize.js
+
+## Inputs
+
+## `localize-api-key`
+
+**Required** The Localize.js account API key
+
+## `localize-project-id`
+
+**Required** ID for the Localize.js project to translate
+
+## `file-format`
+
+The file format to use in all push and pull actions
+Default: "json"
+
+## `action`
+
+**Required** The action to peform-- either push or pull
+
+## `type`
+
+The type of translation-- either phrase or glossary
+Default: "phrase"
+
+## Example usage
+
+uses: actions/localizejs-github-action@v1
+with:
+localize-api-key: 'API_KEY'
+localize-project-id: 'my-translated-project'
+action: 'pull'
---
+++ entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/sh -l
+
+API_KEY=$1
+PROJECT_ID=$2
+FORMAT=$3
+ACTION=$4
+TYPE=$5
+
+# Extract messages
+npx @idagio/intl-utils extract {$GITHUB_WORKSPACE}/src {$GITHUB_WORKSPACE}/string.json {$GITHUB_WORKSPACE}/babel.config.js
+
+# Install the Localize API
+pip install --ignore-installed localize
+
+echo "
+api:
+  project: {$PROJECT_ID}
+  token: {$API_KEY}
+format: {$FORMAT}
+type: {$TYPE}
+pull:
+  targets:
+  - tl: {$GITHUB_WORKSPACE}/strings/tl.json
+  - zh-TW: {$GITHUB_WORKSPACE}/strings/zh-TW.json
+  - zh: {$GITHUB_WORKSPACE}/strings/tl.json
+  - ko: {$GITHUB_WORKSPACE}/strings/ko.json
+  - vi: {$GITHUB_WORKSPACE}/strings/vi.json
+  - uk: {$GITHUB_WORKSPACE}/strings/uk.json
+  - ru: {$GITHUB_WORKSPACE}/strings/ru.json
+  - bs: {$GITHUB_WORKSPACE}/strings/bs.json
+  - hi: {$GITHUB_WORKSPACE}/strings/hi.json
+  - ht: {$GITHUB_WORKSPACE}/strings/ht.json
+  - id: {$GITHUB_WORKSPACE}/strings/id.json
+  - th: {$GITHUB_WORKSPACE}/strings/th.json
+  - de: {$GITHUB_WORKSPACE}/strings/de.json
+  - es: {$GITHUB_WORKSPACE}/strings/es.json
+  - ar: {$GITHUB_WORKSPACE}/strings/ar.json
+  - pt: {$GITHUB_WORKSPACE}/strings/pt.json
+  - ja: {$GITHUB_WORKSPACE}/strings/ja.json
+  - fr: {$GITHUB_WORKSPACE}/strings/fr.json
+  - fr-CA: {$GITHUB_WORKSPACE}/strings/fr-CA.json
+  - haw: {$GITHUB_WORKSPACE}/strings/haw.json
+  - am: {$GITHUB_WORKSPACE}/strings/am.json
+  - hmn: {$GITHUB_WORKSPACE}/strings/hmn.json
+  - pa: {$GITHUB_WORKSPACE}/strings/pa.json
+push:
+  sources:
+  - file: {$GITHUB_WORKSPACE}/strings/en.json
+" >> ~/.localize/config.yml
+
+if($ACTION == "push")
+  localize push
+elif($ACTION == "push")
+  localize pull
+fi
