2dfdf5b3027faa3a95b5798d00eb373137300c3f
Robert-Gee <robert.gee@submittable.com>
2024-01-17 13:22:20 -0700

    add $GITHUB_WORKSPACE path to script.

--- .github/workflows/localize-reformat-restructure-json/entrypoint.sh
+++ .github/workflows/localize-reformat-restructure-json/entrypoint.sh
@@ -9,9 +9,9 @@ LANGUAGES_ARR=$(echo $LANGUAGES | tr "," "\n")
 
 for lang in $LANGUAGES_ARR
 do
-  objects=$(jq -r '.dictionary[] | "\"" + .phraseKey + "\"" + ":", "\"" + .translation[].value +"\"" + ","' $INPUTPATH/$lang.json)
+  objects=$(jq -r '.dictionary[] | "\"" + .phraseKey + "\"" + ":", "\"" + .translation[].value +"\"" + ","' $GITHUB_WORKSPACE/$INPUTPATH/$lang.json)
   trimmed=$(echo $objects | sed 's/.$//')
-  mkdir $OUTPUTPATH
-  mkdir $OUTPUTPATH/$lang
-  echo '{' $trimmed '}' | jq -r '.' > $OUTPUTPATH/$lang/translation.json
+  mkdir $GITHUB_WORKSPACE/$OUTPUTPATH
+  mkdir $GITHUB_WORKSPACE/$OUTPUTPATH/$lang
+  echo '{' $trimmed '}' | jq -r '.' > $GITHUB_WORKSPACE/$OUTPUTPATH/$lang/translation.json
 done
