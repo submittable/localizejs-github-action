111d4006ffb3533020ca955ee3ff4df14793823c
Robert-Gee <robert.gee@submittable.com>
2024-01-18 12:28:04 -0700

    remove reformatting code.

--- .github/workflows/localize-reformat-restructure-json/entrypoint.sh
+++ .github/workflows/localize-reformat-restructure-json/entrypoint.sh
@@ -9,9 +9,7 @@ LANGUAGES_ARR=$(echo $LANGUAGES | tr "," "\n")
 
 for lang in $LANGUAGES_ARR
 do
-  objects=$(jq -r '.dictionary[] | "\"" + .phraseKey + "\"" + ":", "\"" + .translation[].value +"\"" + ","' $GITHUB_WORKSPACE/$INPUTPATH/$lang.json)
-  trimmed=$(echo $objects | sed 's/.$//')
   mkdir $GITHUB_WORKSPACE/$OUTPUTPATH
   mkdir $GITHUB_WORKSPACE/$OUTPUTPATH/$lang
-  echo '{' $trimmed '}' | jq -r '.' > $GITHUB_WORKSPACE/$OUTPUTPATH/$lang/translation.json
+  mv $GITHUB_WORKSPACE/$INPUTPATH/$lang.json $GITHUB_WORKSPACE/$OUTPUTPATH/translation.json
 done
